8cce49a6d652fd43aaf27e07d3f505ec642fef2c
Jinhan Park <jinhan92park@gmail.com>
2017-06-29 18:14:41 +0900

    tokenizer done

---
+++ my_tokenizer.py
@@ -0,0 +1,21 @@
+import re
+import string
+
+def tokenizer(expr):
+    patterns = []
+    
+    func_pattern = ["sin\(", 'cos\(', 'tan\(', 'arc', 'pow\(', 'log\(']
+    const_pattern = ["e", "pi"]
+    op_pattern = ['\+', '\*', '\-', '\/']
+    num_pattern= ["\(|\)|0\.[0-9]*|\.[0-9]*|0|[1-9][0-9]*\.?[0-9]*"]
+    
+    patterns.extend(func_pattern)
+    patterns.extend(const_pattern)
+    patterns.extend(op_pattern)
+    patterns.extend(num_pattern)
+    
+    combined = string.join(patterns, '|')
+    pattern = '('+ combined + ')'
+    expr = expr.replace(' ', '')
+    tokens = re.split(pattern, expr)
+    return tokens
